17af3912dd5fb569fec6dc35d0f0a5b729923164
zhendery <zhendery@qq.com>
2025-01-03 01:05:07 +0800

    fix: 隐藏文件需要单独复制

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,6 +4,7 @@ set -eu
 # 如果是初次安装，则把项目移动到html下进行持久化，主要是.env和installed.lock，以及本地存储中已上传的图片
 if [ -e '/var/www/lsky' ]; then
     cp -rf /var/www/lsky/* /var/www/html/
+    cp -rf /var/www/lsky/.env.example /var/www/html/.env.example # 隐藏文件需要单独复制
     rm -rf /var/www/lsky
 fi
 
