a4a748f4bf1b02add4e675e9c69a5005f9cfb925
zhendery <zhendery@qq.com>
2025-01-03 00:33:13 +0800

    fix: 修复数据持久化bug
    不能单独挂载.env，因为这样的话还需要挂载installed.lock，否则容器重建时会提示安装；
    也不能判断是否存在.env，存在就创建一个lock，因为.env要持久化那么一定是存在的；要么就判断.env是否存在内容也行；
    但最终决定还是直接挂载整个html，就防止还是漏掉了别的什么需要持久化的文件导致数据丢失，也方便后续直接修改项目php文件进行测试。

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eu
 
+# 如果是初次安装，则把项目移动到html下进行持久化，主要是.env和installed.lock，以及本地存储中已上传的图片
+if [ -e '/var/www/lsky' ]; then
+    cp -rf /var/www/lsky/* /var/www/html/
+    rm -rf /var/www/lsky
+fi
+
 chown -R www-data /var/www/html
 chgrp -R www-data /var/www/html
 chmod -R 755 /var/www/html/
